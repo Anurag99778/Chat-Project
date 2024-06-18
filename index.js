91080435554ea37f29422fb070539324ce9532dd
Anurag99778 <138089599+Anurag99778@users.noreply.github.com>
2024-06-18 18:29:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
 const app = express()
-// app.use(cors({
-//     origin : process.env.FRONTEND_URL,
-//     credentials : true
-// }))
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
 app.use(express.json())
 app.use(cookiesParser())
 
